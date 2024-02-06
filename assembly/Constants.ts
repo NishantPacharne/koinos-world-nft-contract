35da46e3dccea32ff0a324e171d6579c13a62b45
nishant <nishantpacharne16@gmail.com>
2024-02-06 11:57:43 +0530

    mainnet

--- assembly/Constants.ts
+++ assembly/Constants.ts
@@ -6,8 +6,9 @@ export namespace Constants {
   export const MINT_PRICE: u64 = 0;
   export const MINT_FEE: bool = false;
   export const MAX_SUPPLY: u64 = 210;
-  export const URI: string = "ipfs://bafybeifbxfsxdv5fhs3rbougveaikooybutsj655mziuhlqpjmd73ytjw4/";
-  export const OWNER: Uint8Array = Base58.decode("1NPvJuEDhjLuW3VbGqAQe4MdSWSB3YWxK1");
+  // export const URI: string = "ipfs://bafybeifbxfsxdv5fhs3rbougveaikooybutsj655mziuhlqpjmd73ytjw4/";
+  export const URI: string = "https://bafybeigrjohdepoqo4c4uqerwsfklag2de5bpbswin65grbvhbci2jjy24.ipfs.nftstorage.link/";
+  export const OWNER: Uint8Array = Base58.decode("1D62Bo53T3V6KN4nFwsF1hicDMYs6f13JB");
 
   // token mint
   export const TOKEN_PAY: Uint8Array = Base58.decode("");
